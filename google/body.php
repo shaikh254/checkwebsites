3aa74e0a35d1e28c4c01daa531337109299a2285
M.A Shaikh <76864675+shaikh254@users.noreply.github.com>
2024-01-09 19:17:14 +0500

    Add files via upload

---
+++ google/body.php
@@ -0,0 +1,2 @@
+<noscript><iframe src=" https://www.googletagmanager.com/ns.html?id=GTM-PCGF6WC9 "
+altura="0" ancho="0" estilo="display:none;visibilidad:hidden"></iframe></noscript>
---
+++ google/header.php
@@ -0,0 +1,5 @@
+<script>(función(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+nueva Fecha().getTime(),evento:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+' https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f) ;
+})(ventana,documento,'script','dataLayer','GTM-PCGF6WC9');</script>
